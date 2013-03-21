ebad698b7318374e3d6063e8dfec29eaf6750643
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-03-21 18:43:20 +0700

    Initial commit (module copied from Carp::Source, name adjusted)

---
+++ lib/Carp/Always/SyntaxHighlightSource.pm
@@ -0,0 +1,74 @@
+package Carp::Always::SyntaxHighlightSource;
+
+use 5.010001;
+use strict;
+use warnings;
+use Carp::SyntaxHighlightSource;
+
+# VERSION
+
+our %options;
+
+sub import {
+    my $class = shift;
+    %options = @_;
+}
+
+sub _warn {
+    if ($_[-1] =~ /\n$/s) {
+        my $arg = pop @_;
+        $arg =~ s/ at .*? line .*?\n$//s;
+        push @_, $arg;
+    }
+    $Carp::SyntaxHighlightSource::CarpLevel = 1;
+    warn Carp::SyntaxHighlightSource::longmess_heavy(join('', grep { defined } @_), %options);
+}
+
+sub _die {
+    if ($_[-1] =~ /\n$/s) {
+        my $arg = pop @_;
+        $arg =~ s/ at .*? line .*?\n$//s;
+        push @_, $arg;
+    }
+    $Carp::SyntaxHighlightSource::CarpLevel = 1;
+    die Carp::SyntaxHighlightSource::longmess_heavy(join('', grep { defined } @_), %options);
+}
+
+my %OLD_SIG;
+
+BEGIN {
+    @OLD_SIG{qw(__DIE__ __WARN__)} = @SIG{qw(__DIE__ __WARN__)};
+    $SIG{__DIE__} = \&_die;
+    $SIG{__WARN__} = \&_warn;
+}
+
+END {
+    no warnings 'uninitialized';
+    @SIG{qw(__DIE__ __WARN__)} = @OLD_SIG{qw(__DIE__ __WARN__)};
+}
+
+1;
+#ABSTRACT: Carp::Always, but show syntax-highlighted source code context
+
+=head1 SYNOPSIS
+
+ % perl -MCarp::Always::SyntaxHighlightSource script.pl
+
+
+=head1 DESCRIPTION
+
+
+=head1 CREDITS
+
+Modified from L<Carp::Source::Always>.
+
+
+=head1 SEE ALSO
+
+L<Carp::Always>
+
+L<Carp::Source> and L<Carp::Source::Always>
+
+L<carpa>
+
+=cut
